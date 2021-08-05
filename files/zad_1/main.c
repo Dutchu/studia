9fc99b631550e03603c5bb2885ec59111bf53cab
blaszba2 <Bartlomiej.Blaszczyk2@orange.com>
2021-08-05 20:47:42 +0200

    Configured CMake for multiple projecs

---
+++ files/zad_1/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, zad_1!\n");
+    return 0;
+}
---
+++ files/zad_2/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Zad_2!\n");
+    return 0;
+}
