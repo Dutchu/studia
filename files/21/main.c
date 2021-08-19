9bd0e07e137cc710effd8cae999cf9181914ea56
blaszba2 <Bartlomiej.Blaszczyk2@orange.com>
2021-08-19 22:27:22 +0200

    Exercise 21 completed

--- files/21/main.c
+++ files/21/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+int file_size_from_file(FILE *f) {
+
+    if (f == NULL) {
+        return -2;
+    }
+
+    long starting_position = ftell(f);
+
+    if (fseek(f, 0L, SEEK_END) != 0) {
+        return -1;
+    }
+    int file_size = ftell(f);
+
+    fseek(f, starting_position, SEEK_SET);
+
+    return file_size;
+}
+
 
 void readStdInput(char *path) {
     scanf("%30s", path);
@@ -18,7 +36,7 @@ int readFile(FILE *fp_indexes, FILE *fp_letters) {
 
     while (fscanf(fp_indexes, "%d", &index) > 0) {
 //        printf("readFile: index = %d\n", index);
-        fseek(fp_letters, index + 1, SEEK_SET);
+        fseek(fp_letters, index, SEEK_SET);
         fscanf(fp_letters, "%c", &ch);
         printf("%c", ch);
     }
@@ -26,7 +44,8 @@ int readFile(FILE *fp_indexes, FILE *fp_letters) {
     return 1;
 }
 
-int corrupted(FILE *fp) {
+int corruptedNumbers(FILE *fp) {
+
     int ch;
     while ((ch = fgetc(fp)) != '\n') {
         if (ch >= 48 && ch <= 57 || ' ' == ch) {
@@ -36,7 +55,32 @@ int corrupted(FILE *fp) {
     return 0;
 }
 
+int corruptedIndexes(FILE *fp) {
+    int min = 9999;
+    int max = -9999;
+    int tmp;
+
+    while (fscanf(fp, "%d", &tmp) > 0) {
+        if (max < tmp) max = tmp;
+        if (min > tmp) min = tmp;
+//        printf("corruptedIndexes:\n- tmp = %d\n- min = %d\n- max = %d\n", tmp, min, max);
+    }
+
+//    printf("corruptedIndexes [if]: ftell(fp)=%d > min=%d || file_size_from_file(fp)=%d < max=%d\n", ftell(fp), min, file_size_from_file(fp)-1, max);
+    if (ftell(fp) > min || max > file_size_from_file(fp) -1) {
+//        printf("corruptedIndexes [if]: ftell(fp)=%d > min=%d || file_size_from_file(fp)=%d < max=%d\n", ftell(fp), min, file_size_from_file(fp)-1, max);
+        rewind(fp);
+        return 1;
+    }
+
+    rewind(fp);
+    return 0;
+}
+
 int main() {
+
+
+
     FILE *fp_indexes;
     FILE *fp_letters;
 //    char *path = "c:\\temp\\coded.txt";
@@ -55,8 +99,15 @@ int main() {
         return 4;
     }
 
-    /*if file corrupted*/
-    if (corrupted(fp_indexes)) {
+    /*if file corruptedNumbers*/
+    if (corruptedNumbers(fp_indexes)) {
+        printf("File corrupted\n");
+        fclose(fp_indexes);
+        fclose(fp_letters);
+        return 6;
+    }
+
+    if (corruptedIndexes(fp_indexes)) {
         printf("File corrupted\n");
         fclose(fp_indexes);
         fclose(fp_letters);
