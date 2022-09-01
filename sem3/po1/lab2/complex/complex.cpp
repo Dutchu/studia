55191b2845524b13044e667a904402e2485f06ec
BartlomiejB <blaszczyk17@gmail.com>
2022-09-01 21:06:54 +0200

    lab1-complex finished

--- sem3/po1/lab2/complex/complex.cpp
+++ sem3/po1/lab2/complex/complex.cpp
@@ -1,52 +1,62 @@
-#include <stdio.h>
+#include <iostream>
+#include <cmath>
 
-struct node
-{
-    int data;
-    struct node *next;
-    struct node *prev;
-};
+class Complex {
+private:
+    double re;
+    double im;
 
-struct list
-{
-    int counter;
-    struct node *head;
-    struct node *tail;
-};
-
-void inserthead(struct list *list, int data)
-{
-    struct node *node;
-    node = createnode(data);
-    list->counter += 1;
-    list->head = node;
-    
-    
-}
-
-
-struct node* createnode(int data)
-{
-    struct node *node;
-    return node;
-}
-
-void printlist(struct list *list)
-{
-}
-
-int main()
-{
-    struct list *list;
-    for (int i = 0; i < 10; i++)
-    {
-        inserthead(list, (i + 3) / 2);
+public:
+    Complex(double re = 1., double im = 1.) {
+        this->re = re;
+        this->im = im;
     }
 
-    struct node n;
-    n.data = 1;
-    n.next =
+    void print() {
+        std::cout << this->re;
+
+        if (this->im > 0) {
+            std::cout << " + " << this->im << "i";
+        } else {
+            std::cout << " - " << abs(this->im) << "i";
+        }
+    }
+
+    void add(Complex c) {
+        this->re = this->re + c.re;
+        this->im = this->im + c.im;
+    }
+
+    void sub(Complex c) {
+        this->re = this->re - c.re;
+        this->im = this->im - c.im;
+    }
+
+    void mul(Complex c) {
+        this->re = (this->re * c.re) - (this->im * c.im);
+        this->im = (this->re * c.im) + (this->im * c.re);
+    }
+
+    void div(Complex c) {
+        double divisor = std::pow(c.re, 2) + std::pow(c.im, 2);
+        if (divisor == 0) {
+            std::cout << "Can't divide by zero! Operation canceled." << std::endl;
+            return;
+        }
+        this->re = ((this->re * c.re) + (this->im * c.im)) / divisor;
+        this->im = ((this->im * c.re) - (this->re * c.im)) / divisor;
+
+    }
+
+};
 
 
+int main() {
+    Complex complex = Complex();
+    complex.add(Complex(1, 2));
+    complex.sub(Complex(5, 5));
+    complex.mul(Complex(20, -20));
+    complex.div(Complex(0,0));
+    complex.print();
     return 0;
 }
