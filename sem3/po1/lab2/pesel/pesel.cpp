8b967dd6fedd8609b8a2e2097f29cfce62db5242
BartlomiejB <blaszczyk17@gmail.com>
2022-09-03 19:39:05 +0200

    finished lab2-pesel

--- sem3/po1/lab2/pesel/pesel.cpp
+++ sem3/po1/lab2/pesel/pesel.cpp
@@ -1,29 +1,102 @@
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#define DELIMITER ","
+#define FILENAME "../rekordy.txt"
+class Person {
+private:
+    std::string code;
+    std::string name;
+    std::string surname;
+    std::string address;
 
-int isprime(int num)
-{
-    int primeflag = 1;
-    for (int i = 2; i < num / 2; i++) {
-        if (num % i == 0) {
-            printf("For i = %d there was no remainder\n", i);
-            primeflag = 0;
+public:
+    Person(std::string code, std::string name, std::string surname, std::string address) {
+        this->code = code;
+        this->name = name;
+        this->surname = surname;
+        this->address = address;
+    }
+
+    static void load(std::string filename, std::vector<Person*>* destination) {
+
+        // file format : pesel,imie,nazwisko,adres;
+        std::fstream file;
+        file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        size_t pos = 0;
+        std::string line;
+        std::string token;
+        std::vector<std::string> tokens;
+        std::string delimiter = DELIMITER;
+        int data_index;
+        int corrupted;
+        int valid;
+
+        try {
+            file.open(filename, std::ios::in);
+        } catch (std::ifstream::failure &e) {
+          std::cerr <<  "Opening file failed!" << std::endl;
+          std::cerr << e.what();
+        }
+
+        try {
+            while (std::getline(file, line)){
+
+                data_index = 0;
+                corrupted = 0;
+                while ( (pos = line.find(delimiter)) != std::string::npos ) {
+
+
+                    token = line.substr(0, pos);
+                    tokens.push_back(token);
+                    line.erase(0, pos + delimiter.length());
+
+                    if (data_index > 3) {
+                        std::cerr << "Line Corrupted!" << std::endl;
+                        corrupted = 1;
+                        break;
+                    }
+
+                    data_index++;
+                }
+
+                if ( !corrupted && validate(&tokens[0]) ) {
+                    destination->push_back( new Person(tokens[0], tokens[1], tokens[2], tokens[3]) );
+                    tokens.clear();
+                }
+
+            }
+        } catch (std::ifstream::failure &e) {
+            std::cerr <<  "Reading file failed!" << std::endl;
+            std::cerr << e.what() << std::endl;
+        }
+
+        try {
+            file.close();
+        } catch (std::ifstream::failure &e) {
+            std::cerr <<  "Closing file failed!" << std::endl;
+            std::cerr << e.what();
         }
     }
-    return primeflag;
-}
 
+    static bool validate(const std::string *pesel) {
+        if (pesel->length() != 11) return false;
+        return pesel->find_first_not_of("0123456789") == std::string::npos;
+    }
+
+    void print() {
+        std::cout << "Person{" << this->code << ", " << this->name << ", " << this->surname << ", " << this->address << "}" << std::endl;
+    }
+};
 
 int main()
 {
-    int input;
-    printf("Gibe me number: ");
-    scanf("%d", &input);
-    if (isprime(input))
-    {
-        printf("%d Is a pesel\n", input);
-    } else {
-        printf("%d Is not a pesel\n", input);
-    }
-    
+    std::vector<Person*> persons;
+    Person::load(FILENAME, &persons);
+
+    for (auto & person : persons)
+        person -> print();
+
     return 0;
 }
