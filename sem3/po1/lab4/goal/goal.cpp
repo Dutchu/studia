a03162fa267d4989066cfd4a06a7ce165c080682
BartlomiejB <blaszczyk17@gmail.com>
2022-09-04 22:02:45 +0200

    lab4 init

---
+++ sem3/po1/lab4/goal/goal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Person {
+protected:
+    std::string name;
+    std::string last_name;
+    std::string date_of_birth;
+
+public:
+};
+
+
+int main() {
+    return 0;
+}
---
+++ sem3/po1/lab4/resistor/resistor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Resistor {
+private:
+    double resistance;
+};
+
+int main() {
+
+    return 0;
+}
