bfe2d918872b825ed75805b1a805c804a5e4cdc0
BartlomiejB <blaszczyk17@gmail.com>
2022-09-07 18:04:44 +0200

    lab6-sequance completed!

--- sem3/po1/lab6/sequence/sequence.cpp
+++ sem3/po1/lab6/sequence/sequence.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <cmath>
+
+template<typename T> class Sequence {
+public:
+    Sequence<T>()= default;
+
+    T get(T n) {
+        if (n <= 1) return n;
+        return get(n - 1) + get(n - 2);
+    }
+
+    bool contains(T e) {
+        return square(5 * e * e + 4) || square(5 * e * e - 4);
+    }
+
+    bool square(T n) {
+        T sqr = sqrt(n);
+        return (sqr * sqr == n);
+    }
+};
 
 int main() {
+    auto fib1 = Sequence<int>();
+    auto fib2 = Sequence<long>();
+    auto fib3 = Sequence<float>();
 
+    std::cout << fib1.get(7) << std::endl;
+    std::cout << fib2.get(7) << std::endl;
+    std::cout << fib3.get(7.) << std::endl;
 
+    std::cout << fib1.contains(7) << std::endl;
+    std::cout << fib2.contains(13) << std::endl;
+    std::cout << fib3.contains(20.0) << std::endl;
+    return 0;
 }
