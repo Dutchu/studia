9c5f8c7befe68bc31f6e6516fef1d69527002cda
Bartlomiej Blaszczyk <236382@edu.p.lodz.pl>
2024-04-20 15:39:17 +0200

    conversion to JUnit5 testing with @ParameterizedTest

--- sem6/zpo/lab1/src/test/java/pl/zpo/lab1/NumberConverterTest.java
+++ sem6/zpo/lab1/src/test/java/pl/zpo/lab1/NumberConverterTest.java
@@ -1,34 +1,37 @@
 package pl.zpo.lab1;
 
 import lombok.extern.java.Log;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.logging.Logger;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NumberConverterTest {
 
     private static final Logger LOG = Logger.getLogger(NumberConverter.class.getName());
     private NumberConverter nc;
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         LOG.info("Rozpoczynamy Testy");
         nc = new NumberConverter();
     }
 
-    @After
+    @AfterEach
     public void tearDown() throws Exception {
         LOG.info("Konczymy testy Testy");
     }
 
     @Test
     public void roman2arabic() {
-        assertEquals("Assert if I is 1", 1, nc.roman2arabic("I"));
-        assertEquals("Assert if I is 1", 5, nc.roman2arabic("V"));
-        assertEquals("Assert if I is 1", 10, nc.roman2arabic("X"));
+        assertEquals( 1, nc.roman2arabic("I"), "Assert if I is 1");
+        assertEquals(5, nc.roman2arabic("V"), "Assert if I is 1");
+        assertEquals(10, nc.roman2arabic("X"), "Assert if I is 1");
     }
 
     @Test
@@ -43,4 +46,17 @@ public class NumberConverterTest {
         });
         assertEquals("Liczba nie jest znakiem rzymskim", thrown.getMessage());
     }
+
+    @ParameterizedTest
+//    @ValueSource(strings = {"'VII':7", "'MCMXCV':1995", "'MMX':2010"})
+    @CsvSource({
+            "VII, 7",
+            "MCMXCV, 1995",
+            "MMX, 2010"
+    })
+    public void roman2arabicExceptions(String input, int expected) {
+        assertEquals(expected, nc.roman2arabic(input));
+    }
+
+
 }
