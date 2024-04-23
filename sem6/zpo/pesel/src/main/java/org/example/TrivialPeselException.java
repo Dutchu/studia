e498753bb1d90c6ca9eb18348c6d051605b2d414
BartlomiejB <blaszczyk17@gmail.com>
2024-04-23 23:57:09 +0200

    New excpetions added to solution

---
+++ sem6/zpo/pesel/src/main/java/org/example/TrivialPeselException.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class TrivialPeselException extends IllegalArgumentException {
+
+}
---
+++ sem6/zpo/pesel/src/main/java/org/example/WrongValuesPeselException.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class WrongValuesPeselException extends IllegalArgumentException {
+}
