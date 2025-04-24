a819b4d7695cb3c4a3f5f9b5d993a96dcd8f3d69
BartlomiejB <blaszczyk17@gmail.com>
2025-04-24 12:54:07 +0200

    CMakeList build files added to repo

--- sem2/pp2/tests/translate/translator.c
+++ sem2/pp2/tests/translate/translator.c
@@ -4,9 +4,29 @@
 
 #include "translator.h"
 #include "stddef.h"
+#include "stdio.h"
+
+#define LINES_COUNT 100
+#define LINE_SIZE 50
 
 struct word_t** read_words(const char* filename, enum error_t* errcode) {
     if (!filename || !errcode) return NULL;
+
+    FILE *fp;
+
+    if ((fp = fopen(filename, "r")) == NULL) {
+        printf("[read_words] fopen() failed!\n");
+        return NULL;
+    }
+
+    char line_buff[LINE_SIZE];
+
+    if (fgets(line_buff, LINE_SIZE, fp) != line_buff) {
+        printf("[read_words] fgets() failed!\n");
+        return NULL;
+    }
+
+    
 
     return NULL;
 }
