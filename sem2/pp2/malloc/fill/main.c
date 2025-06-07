590973b2ab670c1d023c3fc6a24138bac2019aa6
BartlomiejB <blaszczyk17@gmail.com>
2025-06-07 13:00:01 +0200

    finished pp2 lab4 ex1

--- sem2/pp2/malloc/fill/main.c
+++ sem2/pp2/malloc/fill/main.c
@@ -1,8 +1,29 @@
 //
 // Created by Bartek on 07.06.2025.
 //
-#include "utils.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 100
 
 int main() {
-    hello_world();
+
+    int *ptr = malloc(SIZE * sizeof(*ptr));
+    if (ptr == NULL) {
+        printf("Failed to allocate memory");
+        exit(8);
+    }
+
+    for (int i = 0; i < SIZE; ++i) {
+        *(ptr + i) = i;
+    }
+
+    for (int i = 0; i < SIZE; ++i) {
+        printf("%d ", *(ptr+i));
+    }
+
+    free(ptr);
+
+    return 0;
 }
