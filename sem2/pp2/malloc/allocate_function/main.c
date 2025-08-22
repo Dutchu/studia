d2ec239e65791478f9b4431280eb5e72b6a5e42e
BartlomiejB <blaszczyk17@gmail.com>
2025-08-22 14:37:05 +0200

    init pp2 lab4 ex3

---
+++ sem2/pp2/malloc/allocate_function/main.c
@@ -0,0 +1,51 @@
+//
+// Created by Bartek on 22.08.2025.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float* create_array(int N) {
+    if (N <= 0) {
+        return NULL;
+    }
+    float* array = (float*)malloc(N * sizeof(float));
+    return array;
+}
+
+int main() {
+    int N;
+    printf("Podaj rozmiar tablicy: ");
+    if (scanf("%d", &N) != 1) {
+        fprintf(stderr, "Incorrect input\n");
+        return 1;
+    }
+
+    if (N <= 0) {
+        fprintf(stderr, "Incorrect input data\n");
+        return 2;
+    }
+
+    float* array = create_array(N);
+    if (array == NULL) {
+        fprintf(stderr, "Failed to allocate memory\n");
+        return 8;
+    }
+
+    printf("Podaj liczby: ");
+    for (int i = 0; i < N; ++i) {
+        if (scanf("%f", array + i) != 1) {
+            fprintf(stderr, "Incorrect input\n");
+            free(array);
+            return 1;
+        }
+    }
+
+    for (int i = N - 1; i >= 0; --i) {
+        printf("%.4f ", *(array + i));
+    }
+    printf("\n");
+
+    free(array);
+    return 0;
+}
