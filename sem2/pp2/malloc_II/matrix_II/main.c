430ef9777190f6c05573fa3f643c71b4eba63277
BartlomiejB <blaszczyk17@gmail.com>
2025-08-27 20:03:08 +0200

    init pp2 lab5 ex5

--- sem2/pp2/malloc_II/matrix_II/main.c
+++ sem2/pp2/malloc_II/matrix_II/main.c
@@ -1,130 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-struct matrix_t {
-    int **ptr;
-    int width;
-    int height;
-};
-
-
-int matrix_create(struct matrix_t *m, int width, int height);
-int matrix_read(struct matrix_t *m);
-void matrix_display(const struct matrix_t *m);
-void matrix_destroy(struct matrix_t *m);
-
+#include <string.h>
+#include "matrix_utils.h"
 
 int main(void) {
-    struct matrix_t matrix;
     int width, height;
-
     printf("Podaj szerokość i wysokość: ");
     if (scanf("%d %d", &width, &height) != 2) {
         printf("incorrect input\n");
         return 1;
     }
 
-    
-    int create_status = matrix_create(&matrix, width, height);
-    if (create_status == 1) {
+    if (width < 1 || height < 1) {
         printf("incorrect input data\n");
         return 2;
     }
-    if (create_status == 2) {
+
+    struct matrix_t *matrix = matrix_create_struct(width, height);
+    if (matrix == NULL) {
         printf("Failed to allocate memory\n");
         return 8;
     }
 
-    
-    printf("Podaj wartości: \n");
-    int read_status = matrix_read(&matrix);
-    if (read_status != 0) {
+    printf("Podaj liczby: \n");
+    if (matrix_read(matrix) != 0) {
         printf("incorrect input\n");
-        matrix_destroy(&matrix);
+        matrix_destroy_struct(&matrix);
         return 1;
     }
 
-    
-    matrix_display(&matrix);
-    matrix_destroy(&matrix);
+    char *filename = (char*)malloc(40 * sizeof(char));
+    if (filename == NULL) {
+        printf("Failed to allocate memory\n");
+        matrix_destroy_struct(&matrix);
+        return 8;
+    }
+
+    printf("Podaj sciezke do pliku:\n");
+    scanf("%39s", filename);
+
+    struct matrix_t *transposed_matrix = matrix_transpose(matrix);
+    if (transposed_matrix == NULL) {
+        printf("Failed to allocate memory\n");
+        free(filename);
+        matrix_destroy_struct(&matrix);
+        return 8;
+    }
+
+    int save_status = -1;
+    if (strstr(filename, ".bin") == filename + strlen(filename) - 4) {
+        save_status = matrix_save_b(transposed_matrix, filename);
+    } else if (strstr(filename, ".txt") == filename + strlen(filename) - 4) {
+        save_status = matrix_save_t(transposed_matrix, filename);
+    } else {
+        printf("Unsupported file format\n");
+        free(filename);
+        matrix_destroy_struct(&transposed_matrix);
+        matrix_destroy_struct(&matrix);
+        return 7;
+    }
+
+    if (save_status == 2) {
+        printf("Couldn't create file\n");
+        free(filename);
+        matrix_destroy_struct(&transposed_matrix);
+        matrix_destroy_struct(&matrix);
+        return 5;
+    } else if (save_status == 0) {
+        printf("File saved\n");
+    }
+
+    free(filename);
+    matrix_destroy_struct(&transposed_matrix);
+    matrix_destroy_struct(&matrix);
 
     return 0;
-}
-
-int matrix_create(struct matrix_t *m, int width, int height) {
-    if (m == NULL || width < 1 || height < 1) {
-        return 1; 
-    }
-
-    m->width = width;
-    m->height = height;
-    m->ptr = (int **)malloc(height * sizeof(int *));
-
-    if (m->ptr == NULL) {
-        return 2; 
-    }
-
-    
-    for (int i = 0; i < height; ++i) {
-        *(m->ptr + i) = (int *)malloc(width * sizeof(int));
-
-        
-        if (*(m->ptr + i) == NULL) {
-            
-            m->height = i;
-            
-            matrix_destroy(m);
-            return 2; 
-        }
-    }
-
-    return 0; 
-}
-
-int matrix_read(struct matrix_t *m) {
-    if (m == NULL || m->ptr == NULL || m->width < 1 || m->height < 1) {
-        return 1; 
-    }
-
-    for (int i = 0; i < m->height; ++i) {
-        for (int j = 0; j < m->width; ++j) {
-            
-            if (scanf("%d", (*(m->ptr + i) + j)) != 1) {
-                return 2; 
-            }
-        }
-    }
-    return 0; 
-}
-
-void matrix_display(const struct matrix_t *m) {
-    if (m == NULL || m->ptr == NULL || m->width < 1 || m->height < 1) {
-        return; 
-    }
-
-    for (int i = 0; i < m->height; ++i) {
-        for (int j = 0; j < m->width; ++j) {
-            printf("%d ", *(*(m->ptr + i) + j));
-        }
-        printf("\n");
-    }
-}
-
-void matrix_destroy(struct matrix_t *m) {
-    if (m == NULL || m->ptr == NULL) {
-        return; 
-    }
-
-    
-    for (int i = 0; i < m->height; ++i) {
-        free(*(m->ptr + i));
-    }
-    
-    free(m->ptr);
-
-    
-    m->ptr = NULL;
-    m->width = 0;
-    m->height = 0;
 }
---
+++ sem2/pp2/malloc_II/matrix_II/matrix_utils.c
@@ -0,0 +1,146 @@
+#include "matrix_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct matrix_t* matrix_create_struct(int width, int height) {
+    if (width < 1 || height < 1) {
+        return NULL;
+    }
+
+    struct matrix_t* m = (struct matrix_t*)malloc(sizeof(struct matrix_t));
+    if (m == NULL) {
+        return NULL;
+    }
+
+    m->width = width;
+    m->height = height;
+    m->ptr = (int **)malloc(height * sizeof(int *));
+
+    if (m->ptr == NULL) {
+        free(m); 
+        return NULL;
+    }
+    
+    for (int i = 0; i < height; ++i) {
+        *(m->ptr + i) = (int *)malloc(width * sizeof(int));
+        if (*(m->ptr + i) == NULL) {
+            
+            for (int j = 0; j < i; ++j) {
+                free(*(m->ptr + j));
+            }
+            free(m->ptr);
+            free(m);
+            return NULL;
+        }
+    }
+    return m;
+}
+
+void matrix_destroy_struct(struct matrix_t **m) {
+    if (m == NULL || *m == NULL) {
+        return;
+    }
+
+    if ((**m).ptr != NULL) {
+        for (int i = 0; i < (**m).height; ++i) {
+            free(*((**m).ptr + i));
+        }
+        free((**m).ptr);
+    }
+
+    free(*m);
+    *m = NULL;
+}
+
+int matrix_read(struct matrix_t *m) {
+    if (m == NULL || m->ptr == NULL || m->width < 1 || m->height < 1) {
+        return 1;
+    }
+    for (int i = 0; i < m->height; ++i) {
+        for (int j = 0; j < m->width; ++j) {
+            if (scanf("%d", (*(m->ptr + i) + j)) != 1) {
+                return 2;
+            }
+        }
+    }
+    return 0;
+}
+
+struct matrix_t* matrix_transpose(const struct matrix_t *m) {
+    if (m == NULL || m->ptr == NULL) {
+        return NULL;
+    }
+
+    struct matrix_t* transposed_m = matrix_create_struct(m->height, m->width);
+    if (transposed_m == NULL) {
+        return NULL;
+    }
+    
+    for (int i = 0; i < m->height; ++i) {
+        for (int j = 0; j < m->width; ++j) {
+            *(*(transposed_m->ptr + j) + i) = *(*(m->ptr + i) + j);
+        }
+    }
+    return transposed_m;
+}
+
+
+int matrix_save_t(const struct matrix_t *m, const char *filename) {
+    if (m == NULL || m->ptr == NULL || filename == NULL) {
+        return 1;
+    }
+
+    FILE *f = fopen(filename, "w");
+    if (f == NULL) {
+        return 2;
+    }
+
+    if (fprintf(f, "%d %d\n", m->width, m->height) < 0) {
+        fclose(f);
+        return 3;
+    }
+
+    for (int i = 0; i < m->height; ++i) {
+        for (int j = 0; j < m->width; ++j) {
+            if (fprintf(f, "%d ", *(*(m->ptr + i) + j)) < 0) {
+                fclose(f);
+                return 3;
+            }
+        }
+        if (fprintf(f, "\n") < 0) {
+            fclose(f);
+            return 3;
+        }
+    }
+    fclose(f);
+    return 0;
+}
+
+int matrix_save_b(const struct matrix_t *m, const char *filename) {
+    if (m == NULL || m->ptr == NULL || filename == NULL) {
+        return 1;
+    }
+
+    FILE *f = fopen(filename, "wb");
+    if (f == NULL) {
+        return 2;
+    }
+
+    if (fwrite(&(m->width), sizeof(int), 1, f) != 1) {
+        fclose(f);
+        return 3;
+    }
+    if (fwrite(&(m->height), sizeof(int), 1, f) != 1) {
+        fclose(f);
+        return 3;
+    }
+
+    for (int i = 0; i < m->height; ++i) {
+        if (fwrite(*(m->ptr + i), sizeof(int), m->width, f) != (size_t) m->width) {
+            fclose(f);
+            return 3;
+        }
+    }
+    fclose(f);
+    return 0;
+}
